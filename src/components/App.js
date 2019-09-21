de6ea617d2c78ad82c5d5468543df210de349c19
ednaustin <edramirez512@gmail.com>
2019-09-21 14:08:04 -0500

    Working with Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit (term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID bc1f57a2c1a94573bc7bfd94262ae7ccaf0be01fa9acc1ee7b2a07eae30b2ad4'
+        }
+    });
   }
 
   render() {
